b9ddaeff4cd1361e15837668b28f0d85a246cee3
sokolov <sokolovsergeysse@gmail.com>
2025-03-03 20:06:03 +0200

    add object personalMovieDB

--- Lesson_1/js/script.js
+++ Lesson_1/js/script.js
@@ -1,1 +1,19 @@
-console.log('1');
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+let question1 = prompt("Один из последних просмотренных фильмов?", ""),
+    question2 = prompt("На сколько оцените его?", ""),
+    question3 = prompt("Один из последних просмотренных фильмов?", ""),
+    question4 = prompt("На сколько оцените его?", "");
+
+    personalMovieDB.movies[question1] = question2;
+    personalMovieDB.movies[question3] = question4;
+
+console.log(personalMovieDB);
